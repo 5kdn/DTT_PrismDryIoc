6effa1a4c8225d8ef926d30ec7366020736292a6
5kdn <sakuden542+github@gmail.com>
2025-07-03 17:58:51 +0900

    test: テスト修正

---
+++ DcsTranslateTool/Contracts/Providers/IDialogProvider.cs
@@ -0,0 +1,15 @@
+namespace DcsTranslateTool.Contracts.Providers;
+
+/// <summary>
+/// フォルダ選択ダイアログを表示し、選択されたパスを out で返します。
+/// 例外発生時は false を返し、selectedPath は null になります。
+/// </summary>
+/// <param name="initialDirectory">初期表示ディレクトリ</param>
+/// <param name="selectedPath">選択されたパス（キャンセル時は string.Empty）</param>
+/// <returns>ダイアログで OK が押されたら true。それ以外は false を返します。</returns>
+/// <exception cref="System.Runtime.InteropServices.COMException">ダイアログの表示または COM 操作に失敗した場合にスローされます。</exception>
+/// <exception cref="System.Exception">その他の予期しないエラーが発生した場合にスローされます。</exception>
+public interface IDialogProvider
+{
+    bool ShowFolderPicker( string initialDirectory, out string selectedPath );
+}
