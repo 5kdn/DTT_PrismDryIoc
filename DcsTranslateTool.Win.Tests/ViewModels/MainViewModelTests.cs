0426deabb4ade91bb7efe2c3ce1ed28c3a88217c
5kdn <sakuden542+github@gmail.com>
2025-07-16 20:49:57 +0900

    tests: Add Tests for MainViewModel tests: Add Tests for UploadViewModel

---
+++ DcsTranslateTool.Win.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,35 @@
+﻿using DcsTranslateTool.Core.Contracts.Services;
+using DcsTranslateTool.Core.Services;
+using DcsTranslateTool.Share.Contracts.Services;
+using DcsTranslateTool.Win.Contracts.Services;
+using DcsTranslateTool.Win.Services;
+using DcsTranslateTool.Win.ViewModels;
+
+using Moq;
+
+using Xunit;
+
+namespace DcsTranslateTool.Win.Tests.ViewModels;
+
+public class UploadViewModelTests {
+    private readonly Container _container;
+
+    public UploadViewModelTests() {
+        _container = new Container();
+        _container.Register<IRegionManager, RegionManager>( Reuse.Singleton );
+        _container.Register<IAppSettingsService, AppSettingsService>( Reuse.Singleton );
+        _container.Register<IFileService, FileService>( Reuse.Transient );
+        _container.RegisterDelegate<IRepositoryService>( s => Mock.Of<IRepositoryService>() );
+        // ViewModels
+        _container.Register<UploadViewModel>( Reuse.Singleton );
+    }
+
+    [Fact( DisplayName = "UploadViewModelが正常に生成できる" )]
+    public void TestUploadViewModelCreation() {
+        // Arrange & Act
+        var vm = _container.Resolve<UploadViewModel>();
+
+        // Assert
+        Assert.NotNull( vm );
+    }
+}
---
+++ DcsTranslateTool.Win.Tests/ViewModels/UploadViewModelTests.cs
@@ -0,0 +1,35 @@
+﻿using DcsTranslateTool.Core.Contracts.Services;
+using DcsTranslateTool.Core.Services;
+using DcsTranslateTool.Share.Contracts.Services;
+using DcsTranslateTool.Win.Contracts.Services;
+using DcsTranslateTool.Win.Services;
+using DcsTranslateTool.Win.ViewModels;
+
+using Moq;
+
+using Xunit;
+
+namespace DcsTranslateTool.Win.Tests.ViewModels;
+
+public class MainViewModelTests {
+    private readonly Container _container;
+
+    public MainViewModelTests() {
+        _container = new Container();
+        _container.Register<IRegionManager, RegionManager>( Reuse.Singleton );
+        _container.Register<IAppSettingsService, AppSettingsService>( Reuse.Singleton );
+        _container.Register<IFileService, FileService>( Reuse.Transient );
+        _container.RegisterDelegate<IRepositoryService>( s => Mock.Of<IRepositoryService>() );
+        // ViewModels
+        _container.Register<MainViewModel>( Reuse.Singleton );
+    }
+
+    [Fact( DisplayName = "MainViewModelが正常に生成できる" )]
+    public void TestMainViewModelCreation() {
+        // Arrange & Act
+        var vm = _container.Resolve<MainViewModel>();
+
+        // Assert
+        Assert.NotNull( vm );
+    }
+}
