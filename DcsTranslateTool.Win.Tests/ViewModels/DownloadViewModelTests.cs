29b5157592e519b52c04d1066db8ee27e98bf766
5kdn <sakuden542+github@gmail.com>
2025-07-16 19:26:08 +0900

    feature: MainViewModelViewModelTestsをDownloadViewModelTestsに変更

--- DcsTranslateTool.Win.Tests/ViewModels/DownloadViewModelTests.cs
+++ DcsTranslateTool.Win.Tests/ViewModels/DownloadViewModelTests.cs
@@ -6,27 +6,28 @@ using DcsTranslateTool.Win.Services;
 using DcsTranslateTool.Win.ViewModels;
 
 using Moq;
+
 using Xunit;
 
 namespace DcsTranslateTool.Win.Tests.ViewModels;
 
-public class MainViewModelTests {
+public class DownloadViewModelTests {
     private readonly Container _container;
 
-    public MainViewModelTests() {
+    public DownloadViewModelTests() {
         _container = new Container();
         _container.Register<IRegionManager, RegionManager>( Reuse.Singleton );
         _container.Register<IAppSettingsService, AppSettingsService>( Reuse.Singleton );
         _container.Register<IFileService, FileService>( Reuse.Transient );
         _container.RegisterDelegate<IRepositoryService>( s => Mock.Of<IRepositoryService>() );
         // ViewModels
-        _container.Register<MainViewModel>( Reuse.Singleton );
+        _container.Register<DownloadViewModel>( Reuse.Singleton );
     }
 
-    [Fact( DisplayName = "MainViewModelが正常に生成できる" )]
-    public void TestMainViewModelCreation() {
+    [Fact( DisplayName = "DownloadViewModelが正常に生成できる" )]
+    public void TestDownloadViewModelCreation() {
         // Arrange & Act
-        var vm = _container.Resolve<MainViewModel>();
+        var vm = _container.Resolve<DownloadViewModel>();
 
         // Assert
         Assert.NotNull( vm );
