9ce68b084f1496a5af87fd86cb39beba9acd0f14
5kdn <sakuden542+github@gmail.com>
2025-07-21 01:01:51 +0900

    fix: AppConfigをNullableに修正

--- DcsTranslateTool.Win/Models/AppConfig.cs
+++ DcsTranslateTool.Win/Models/AppConfig.cs
@@ -4,9 +4,9 @@ namespace DcsTranslateTool.Win.Models;
 /// アプリケーション設定を表すモデル
 /// </summary>
 public class AppConfig {
-    public string ConfigurationsFolder { get; set; }
+    public string ConfigurationsFolder { get; set; } = string.Empty;
 
-    public string AppPropertiesFileName { get; set; }
+    public string AppPropertiesFileName { get; set; } = string.Empty;
 
-    public string PrivacyStatement { get; set; }
+    public string PrivacyStatement { get; set; } = string.Empty;
 }
