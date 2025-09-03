779d27086aa39eff7df65d0ba15cc6ce5c60399d
5kdn <sakuden542+github@gmail.com>
2025-09-03 13:51:15 +0900

    feat: add OpenDirectory method

--- DcsTranslateTool.Win/Contracts/Services/ISystemService.cs
+++ DcsTranslateTool.Win/Contracts/Services/ISystemService.cs
@@ -9,4 +9,20 @@ public interface ISystemService {
     /// </summary>
     /// <param name="url">URL</param>
     void OpenInWebBrowser( string url );
+
+    /// <summary>
+    /// 指定されたパスをエクスプローラーで開く。
+    /// パスがディレクトリでない場合は、ファイルが存在する場合にその親ディレクトリを開く。
+    /// </summary>
+    /// <param name="path">開く対象のディレクトリまたはファイルのパス。</param>
+    /// <exception cref="DirectoryNotFoundException">
+    /// 指定されたパスが存在せず、またファイルの親ディレクトリも取得できなかった場合に発生する。
+    /// </exception>
+    /// <exception cref="System.ComponentModel.Win32Exception">
+    /// エクスプローラーの起動に失敗した場合に発生する。
+    /// </exception>
+    /// <exception cref="ObjectDisposedException">
+    /// プロセスオブジェクトが既に破棄されている場合に発生する。
+    /// </exception>
+    void OpenDirectory( string path );
 }
