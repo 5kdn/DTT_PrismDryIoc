4c51420d9cfab1d76c3963a930778156a4dd17cd
5kdn <sakuden542+github@gmail.com>
2025-09-05 15:55:59 +0900

    feat: implement create pull request dialog (#63)
    * feat: implement CreatePullRequestDialog
    
    * feat: update CreatePullRequestDialog design

--- DcsTranslateTool.Tests/Win/ViewModels/CreatePullRequestDialogViewModelTests.cs
+++ DcsTranslateTool.Tests/Win/ViewModels/CreatePullRequestDialogViewModelTests.cs
@@ -1,16 +1,33 @@
-﻿using DcsTranslateTool.Win.ViewModels;
+﻿using DcsTranslateTool.Core.Contracts.Services;
+using DcsTranslateTool.Win.Contracts.Services;
+using DcsTranslateTool.Win.Services;
+using DcsTranslateTool.Win.ViewModels;
+
+using Moq;
 
 using Xunit;
 
 namespace DcsTranslateTool.Tests.Win.ViewModels;
 public class CreatePullRequestDialogViewModelTests {
+    private readonly Container _container;
+    private readonly Mock<IRepositoryService> mockedRepositoryService;
+
+    public CreatePullRequestDialogViewModelTests() {
+        mockedRepositoryService = new Mock<IRepositoryService>();
+        _container = new Container();
+        _container.Register<IAppSettingsService, AppSettingsService>();
+        _container.RegisterInstance<IRepositoryService>( mockedRepositoryService.Object );
+        // ViewModels
+        _container.Register<CreatePullRequestDialogViewModel>( Reuse.Transient );
+    }
+
     #region CanCreatePR
 
     [Fact]
     [Trait( "Category", "WindowsOnly" )]
     public void CanCreatePRは変更点と同意がいずれも未選択のときFalseになる() {
         // Arrange
-        var vm = new CreatePullRequestDialogViewModel();
+        var vm = _container.Resolve<CreatePullRequestDialogViewModel>();
 
         // Act
         var result = vm.CanCreatePR;
@@ -23,7 +40,7 @@ public class CreatePullRequestDialogViewModelTests {
     [Trait( "Category", "WindowsOnly" )]
     public void CanCreatePRは変更点のみチェックしたときFalseになる() {
         // Arrange
-        var vm = new CreatePullRequestDialogViewModel();
+        var vm = _container.Resolve<CreatePullRequestDialogViewModel>();
         foreach(var kind in vm.PullRequestChangeKinds) kind.IsChecked = true;
 
         // Act
@@ -37,7 +54,7 @@ public class CreatePullRequestDialogViewModelTests {
     [Trait( "Category", "WindowsOnly" )]
     public void CanCreatePRはすべての変更点未チェックで同意のみtrueのときFalseになる() {
         // Arrange
-        var vm = new CreatePullRequestDialogViewModel();
+        var vm = _container.Resolve<CreatePullRequestDialogViewModel>();
         foreach(var item in vm.AgreementItems) item.IsAgreed = true;
 
         // Act
@@ -51,7 +68,7 @@ public class CreatePullRequestDialogViewModelTests {
     [Trait( "Category", "WindowsOnly" )]
     public void CanCreatePRは変更点と同意がともにチェック済みのときTrueになる() {
         // Arrange
-        var vm = new CreatePullRequestDialogViewModel();
+        var vm = _container.Resolve<CreatePullRequestDialogViewModel>();
         vm.PullRequestChangeKinds.First().IsChecked = true;
         foreach(var item in vm.AgreementItems) item.IsAgreed = true;
 
@@ -80,7 +97,7 @@ public class CreatePullRequestDialogViewModelTests {
     [Trait( "Category", "WindowsOnly" )]
     public void PRCommentは値を設定したとき同じ値が取得できるようになる() {
         // Arrange
-        var vm = new CreatePullRequestDialogViewModel();
+        var vm = _container.Resolve<CreatePullRequestDialogViewModel>();
         const string expected = "Test Comment";
 
         // Act
@@ -98,7 +115,7 @@ public class CreatePullRequestDialogViewModelTests {
     [Trait( "Category", "WindowsOnly" )]
     public void SelectedChangeKindsはIsCheckedな変更種別のみ返すようになる() {
         // Arrange
-        var vm = new CreatePullRequestDialogViewModel();
+        var vm = _container.Resolve<CreatePullRequestDialogViewModel>();
         vm.PullRequestChangeKinds[0].IsChecked = true;
         vm.PullRequestChangeKinds[1].IsChecked = false;
         vm.PullRequestChangeKinds[2].IsChecked = true;
