f1d8a978f4f68f0f69d5ba904fb4b7f3e3356735
5kdn <sakuden542+github@gmail.com>
2025-09-07 22:12:22 +0900

    refactor: move PullRequestChangeKindItem to PullRequestChangeKindViewModel (#75)

--- DcsTranslateTool.Win/ViewModels/PullRequestChangeKindViewModel.cs
+++ DcsTranslateTool.Win/ViewModels/PullRequestChangeKindViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace DcsTranslateTool.Win.Models;
-// TODO: VM化
-public class PullRequestChangeKindItem( PullRequestChangeKind kind ) : BindableBase {
+﻿using DcsTranslateTool.Win.Models;
+
+namespace DcsTranslateTool.Win.ViewModels;
+
+public class PullRequestChangeKindViewModel( PullRequestChangeKind kind ) : BindableBase {
+    private bool _isChecked;
+
     public PullRequestChangeKind Kind { get; } = kind;
+
     public string DisplayName => Kind.ToString();
 
-    private bool _isChecked;
     public bool IsChecked {
         get => _isChecked;
         set => SetProperty( ref _isChecked, value );
